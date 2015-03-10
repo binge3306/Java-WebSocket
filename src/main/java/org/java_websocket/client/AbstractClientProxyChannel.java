a0c2ff4f9ea9413994ecdf541ff821cd2c2d9410
wuyinxian124 <wuyinxian124@gmail.com>
2015-03-10 11:07:17 +0800

    测试是否fork项目成功

--- src/main/java/org/java_websocket/client/AbstractClientProxyChannel.java
+++ src/main/java/org/java_websocket/client/AbstractClientProxyChannel.java
@@ -7,6 +7,7 @@ import java.nio.channels.ByteChannel;
 
 import org.java_websocket.AbstractWrappedByteChannel;
 
+
 public abstract class AbstractClientProxyChannel extends AbstractWrappedByteChannel {
 	protected final ByteBuffer proxyHandshake;
 

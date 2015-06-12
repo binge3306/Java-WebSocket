e7df1283c177c6addda240dc591b0e29692297b8
wuyinxian124 <wuyinxian124@gmail.com>
2015-06-12 18:41:53 +0800

    测试push到github

--- src/main/java/org/java_websocket/client/AbstractClientProxyChannel.java
+++ src/main/java/org/java_websocket/client/AbstractClientProxyChannel.java
@@ -7,7 +7,6 @@ import java.nio.channels.ByteChannel;
 
 import org.java_websocket.AbstractWrappedByteChannel;
 
-
 public abstract class AbstractClientProxyChannel extends AbstractWrappedByteChannel {
 	protected final ByteBuffer proxyHandshake;
 
